8d84688b48ac488e3ad13d70f5c86ac62f6adf4b
devesh0099 <deveshbhushan8@gmail.com>
2025-06-12 18:40:58 +0530

    added sphere and marking points and toggles

---
+++ src/app/main/page.tsx
@@ -0,0 +1,10 @@
+import SpaceScene from '../../components/SpaceScene'
+
+export default function Simulator() {
+    return (
+        <div>
+            <h1>Simulator</h1>
+            <SpaceScene />
+        </div>
+    )
+}
